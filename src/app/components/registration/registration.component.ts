5b5a09ff703b20c1b5215fa7ccbbae5f231e20c6
Michael <al.burno@mail.ru>
2020-12-16 22:48:26 +0400

    ver 6.0

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -25,9 +25,10 @@ export class RegistrationComponent implements OnInit {
 
   save(): void {
     this.authService.createUser(this.user).subscribe(data => {
-      console.log(data);
-      this.user = new User();
-    },
+        console.log(data);
+        this.user = new User();
+        this.router.navigate(['/books']);
+      },
       error => console.log(error));
   }
 
