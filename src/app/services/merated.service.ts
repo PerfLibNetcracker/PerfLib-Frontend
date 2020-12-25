72402122baf1827997525168b5702d27f6736bbe
okepro <krukov.99@mail.ru>
2020-12-25 18:38:51 +0400

    Добавил компонент merated

---
+++ src/app/services/merated.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {BookDTO} from '../model/bookDTO';
+import {tap} from "rxjs/operators";
+import {Book} from "../model/Book";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeratedService {
+
+  private URL_ratedByMe = 'http://localhost:8081/api/service/authentication/authenticated/rated/';
+
+  constructor(private http: HttpClient) { }
+
+  getInfoAboutRated(id: string): Observable<any> {
+    return this.http.get(this.URL_ratedByMe + id, { withCredentials: true } );
+  }
+
+}
